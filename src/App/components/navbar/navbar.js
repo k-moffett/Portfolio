5cb327ff5d94928508b0889fc10f0dbfe82b9269
Kurtis Moffett <moffkr91@gmail.com>
2018-06-01 12:51:45 -0700

    qs

--- src/App/components/navbar/navbar.js
+++ src/App/components/navbar/navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './index.css'
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props)
+    }
 
     homepageClick() {
         this.props.displayHomepage()
@@ -15,12 +18,26 @@ export default class Navbar extends Component {
         this.props.displayContact()
     }
 
+    menuBtn() {
+      return(
+          <div className='dropDown'>      
+            <button>                       
+                <div className='menuBtn'></div>
+                <div className='menuBtn'></div>
+                <div className='menuBtn'></div> 
+            </button>
+          </div>
+          )
+    }
+
+
     render() {
         return(
             <div>
-               <button onClick={(e) => {this.homepageClick()}}>Home</button>
-               <button onClick={(e) => {this.portfolioClick()}}>Portfolio</button>
-               <button onClick={(e) => {this.contactClick()}}>Contact</button>
+                <this.menuBtn />
+                <button onClick={(e) => {this.homepageClick()}}>Home</button>
+                <button onClick={(e) => {this.portfolioClick()}}>Portfolio</button>
+                <button onClick={(e) => {this.contactClick()}}>Contact</button>
             </div>
         )
     }
