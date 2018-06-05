b6a4cc2a219b21cf1e93d2f591a52e7ac542f58d
Kurtis Moffett <moffkr91@gmail.com>
2018-06-05 12:19:54 -0700

    qs

--- src/App/components/homepage/main.js
+++ src/App/components/homepage/main.js
@@ -9,12 +9,12 @@ export default class Main extends Component {
         return(
             <Container>
                 <Row>
-                    <Col xs="6">
-                    <img id='profile-pic' src={profilePic} />
-                    </Col>
-                    <Col xs="6">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nisi leo, faucibus ut hendrerit sed, lacinia vel dolor. Nam facilisis lectus et odio auctor pulvinar. Vivamus nec justo quis diam varius fringilla. Aenean fringilla, urna non ornare eleifend, felis dui imperdiet ante, egestas ullamcorper sapien nulla sit amet orci. Curabitur consectetur sem nulla, vel commodo tortor ullamcorper id. Cras nec malesuada velit, a consequat nulla. Aliquam luctus ullamcorper sollicitudin. Fusce sodales hendrerit sagittis. Aliquam scelerisque ac nibh at commodo. Morbi accumsan facilisis eleifend. Duis in urna non lectus iaculis faucibus. Aliquam erat volutpat. Nullam quis lectus vitae purus tempor eleifend ac id ligula. Nunc consectetur volutpat magna sit amet euismod. Praesent tempor tincidunt eros, vel ultricies augue tempor eu.
-                        Etiam egestas vulputate feugiat. Donec ultrices, tellus quis efficitur auctor, tellus velit bibendum nunc, ac vehicula mi nibh vel sapien. Mauris in nisi et sem porttitor tincidunt vel quis sapien. Donec tristique justo quis purus feugiat rhoncus. Aenean dapibus dignissim augue, et blandit justo semper et. Praesent vitae nisi at nisi vulputate commodo eget nec velit. Suspendisse egestas urna eget massa ornare, at congue velit luctus. Fusce scelerisque est eu felis mollis ultrices.
+                    <Col id="main" xs="12">
+                        <img id='profile-pic' src={profilePic} />
+                        <br/>
+                        Web development is my passion. The UCSD Web Development Bootcamp has allowed me to grow as an individual, as well as work as a team player on projects. I am proficient in JavaScript, React JS, RESTful APIs, Node JS, Express JS, HTML 5, CSS, jQuery, AJAX, MySQL, MongoDB, Git, Heroku, AWS, Firebase and more. This not only because of the many hours spent as an individual creating solutions to problem, but also because we can learn so much more by working together to achieve our goals.
+                        <br/>
+                        From building an Express back-end, or crafting components to render in React, what I really enjoy most is solving problems . New problems always create an opportunity to learn, and that is one of the most satisfying aspects of Web Development. As the industry continues to grow and the technologies constantly change, new problems will require new creative solutions. The fact that there is always something more to learn and improve on is what drives me.             
                     </Col>
                 </Row>
             </Container>
